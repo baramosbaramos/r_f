df5a75b1eec27cd8241127405a1c202afbb28227
hariharibarako <sousoubaramos@gmail.com>
2023-02-17 15:17:13 +0900

    Routing add

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -13,9 +14,18 @@ function App() {
       });
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
+  function Page1() {
+    return (
+      <div>
+        <div>
+          <Link className="App-link" to="/">
+            Home
+          </Link>
+          &nbsp;| &nbsp;
+          <Link className="App-link" to="/page2">
+            page2
+          </Link>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -28,36 +38,31 @@ function App() {
         >
           Learn React
         </a>
-        <p>The current time is {currentTime}.</p>
+        <p>The current time is {currentTime}.</p>{" "}
+      </div>
+    );
+  }
+
+  function Page2() {
+    return (
+      <div>
+        <p>This is page2!</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Page1 />} />
+            <Route path="/page2" element={<Page2 />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
 }
 
 export default App;
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
